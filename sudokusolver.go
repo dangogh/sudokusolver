8df393d41155f717be3a0a10786947b066adb7b0
Dan Kirkwood <dangogh@gmail.com>
2018-10-14 18:50:14 +0000

    reads and writes puzzle from file

---
+++ sudokusolver.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+type Cell int
+type Row []Cell
+type Puzzle []Row
+
+func NewPuzzle(r io.Reader) (Puzzle, error) {
+	scanner := bufio.NewScanner(r)
+
+	var rows []Row
+	for scanner.Scan() {
+		var row []Cell
+		for _, c := range scanner.Bytes() {
+			switch {
+			case c == '_':
+				row = append(row, Cell(0))
+			case unicode.IsDigit(rune(c)):
+				row = append(row, Cell(c-'0'))
+			default:
+				continue
+			}
+		}
+		if len(row) != 9 {
+			return Puzzle{}, errors.New("bad row with " + strconv.Itoa(len(row)) + "entries")
+		}
+		rows = append(rows, row)
+	}
+	return Puzzle(rows), nil
+}
+
+func (p Puzzle) String() string {
+	var s []byte
+	for _, r := range []Row(p) {
+		for _, b := range r {
+			if int(b) == 0 {
+				s = append(s, '_')
+			} else {
+				s = append(s, byte(b)+'0')
+			}
+			s = append(s, ' ')
+		}
+		s[len(s)-1] = '\n'
+	}
+	return string(s)
+}
+
+func (p Puzzle) Solve() Puzzle {
+	return p
+}
+
+func main() {
+	for _, fn := range os.Args[1:] {
+		f, err := os.Open(fn)
+		if err != nil {
+			panic(err.Error())
+		}
+		puzzle, err := NewPuzzle(f)
+		puzzle.Solve()
+		fmt.Println(puzzle.String())
+	}
+
+}
