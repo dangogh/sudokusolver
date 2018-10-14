5d6e32bf9d87608568dcdbb99e5d222c9b89b21e
Dan Kirkwood <dangogh@gmail.com>
2018-10-14 19:54:45 +0000

    add Solve to test -- failing

--- sudokusolver_test.go
+++ sudokusolver_test.go
@@ -18,15 +18,32 @@ _ 3 _ 5 _ 7 _ 9 _
 `,
 }
 
-func TestReadPuzzle(t *testing.T) {
-	for _, s := range testPuzzles {
+var solvedPuzzles = []string{
+	`1 2 3 4 5 6 7 8 9
+7 8 9 1 2 3 4 5 6 
+4 5 6 7 8 9 1 2 3
+9 1 2 3 4 5 6 7 8
+6 7 8 9 1 2 3 4 5
+3 4 5 6 7 8 9 1 2
+8 9 1 2 3 4 5 6 7
+5 6 7 8 9 1 2 3 4
+2 3 4 5 6 7 8 9 1
+`,
+}
+
+func TestSolvedPuzzle(t *testing.T) {
+	for i, s := range testPuzzles {
 		r := strings.NewReader(s)
 		p, err := NewPuzzle(r)
 		if err != nil {
 			t.Errorf("error reading puzzle: %s", err.Error())
 		}
 		if p.String() != s {
-			t.Errorf("puzzle in `%s` != puzzle out: `%s`", s, p.String())
+			t.Errorf("puzzle in `\n%s` != puzzle out: `\n%s`", s, p.String())
+		}
+		solved := p.Solve()
+		if solved.String() != solvedPuzzles[i] {
+			t.Errorf("solution: `\n%s` != solved: `\n%s`", solvedPuzzles[i], solved.String())
 		}
 	}
 }
