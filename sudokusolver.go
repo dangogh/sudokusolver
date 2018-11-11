b268570707cd609a1e494bea409c97c023a638d1
Dan Kirkwood <dangogh@gmail.com>
2018-11-11 03:44:30 +0000

    groups initialized

--- sudokusolver.go
+++ sudokusolver.go
@@ -7,28 +7,55 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 type Cell byte
-type Row []Cell
-type Puzzle []Row
+
+func NewCell(val byte) *Cell {
+	cell := Cell(val)
+	return &cell
+}
+
+func (c Cell) String() string {
+	i := byte(c)
+	if i == 0 {
+		return "_"
+	}
+	return string(i + byte('0'))
+}
+
+type Group []*Cell
+
+type Puzzle struct {
+	Rows    []Group
+	Columns []Group
+	Boxes   []Group
+}
 
 func NewPuzzle(r io.Reader) (Puzzle, error) {
 	scanner := bufio.NewScanner(r)
 
-	var rows []Row
+	var p Puzzle
+	p.Rows = make([]Group, 9)
+	p.Columns = make([]Group, 9)
+	p.Boxes = make([]Group, 9)
+
+	var rowIdx int
 	for scanner.Scan() {
-		var row []Cell
-		for _, c := range scanner.Bytes() {
+		var row []*Cell
+		for _, b := range scanner.Bytes() {
+			var c *Cell
 			switch {
-			case c == '_':
-				row = append(row, Cell(0))
-			case unicode.IsDigit(rune(c)):
-				row = append(row, Cell(c-'0'))
+			case b == '_':
+				c = NewCell(0)
+			case unicode.IsDigit(rune(b)):
+				c = NewCell(b - '0')
 			default:
 				continue
 			}
+			row = append(row, c)
 		}
 		if len(row) == 0 {
 			continue
@@ -36,30 +63,58 @@ func NewPuzzle(r io.Reader) (Puzzle, error) {
 		if len(row) != 9 {
 			return Puzzle{}, errors.New("bad row with " + strconv.Itoa(len(row)) + "entries")
 		}
-		rows = append(rows, row)
-	}
-	return Puzzle(rows), nil
-}
-
-func (p Puzzle) String() string {
-	var s []byte
-	for _, r := range []Row(p) {
-		for _, b := range r {
-			if byte(b) == 0 {
-				s = append(s, '_')
-			} else {
-				s = append(s, byte(b)+'0')
-			}
-			s = append(s, ' ')
+		p.Rows[rowIdx] = Group(row)
+		for colIdx, c := range row {
+			p.Columns[colIdx] = append(p.Columns[colIdx], c)
+			boxIdx := 3*(rowIdx/3) + colIdx/3
+			p.Boxes[boxIdx] = append(p.Boxes[boxIdx], c)
 		}
-		s[len(s)-1] = '\n'
+		rowIdx++
 	}
-	return string(s)
+	return p, nil
 }
 
 func (p Puzzle) Solve() Puzzle {
-	fmt.Printf("puzzle is %v\n", p)
+	fmt.Printf("puzzle is\n%v\n", p)
+	groups := append(p.Rows, p.Columns...)
+	groups = append(groups, p.Boxes...)
+
+	/*
+		minAvail := 9
+		changed := false
+			// sort by number of available numbers
+			for _, g := range groups {
+				a := g.Available()
+				switch len(a) {
+				case 0:
+					continue
+				case 1:
+					for _, c := range g {
+						if c.Value() == 0 {
+							c.Fill(a[0])
+							break
+						}
+					}
+				default:
+					if len(a) < len(minGroup.Available()) {
+						minGroup = g
+					}
+				}
+			}
+	*/
 	return p
+}
+
+func (p Puzzle) String() string {
+	var s string
+	for _, r := range p.Rows {
+		var ss []string
+		for _, c := range r {
+			ss = append(ss, c.String())
+		}
+		s += strings.Join(ss, " ") + "\n"
+	}
+	return s
 }
 
 func main() {
