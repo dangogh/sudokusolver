80f7a4ac9db7224335013b00e4c8e332f7206b34
Dan Kirkwood <dangogh@gmail.com>
2018-11-22 00:24:29 +0000

    fix pos test

--- pos_test.go
+++ pos_test.go
@@ -11,7 +11,11 @@ func TestCell(t *testing.T) {
 		box := 3*row + col
 		if row != s.Row() {
 			t.Errorf("pos %d row expected %d, got %d", pos, row, s.Row())
+		}
+		if col != s.Column() {
 			t.Errorf("pos %d col expected %d, got %d", pos, col, s.Column())
+		}
+		if box != s.Box() {
 			t.Errorf("pos %d box expected %d, got %d", pos, box, s.Box())
 		}
 	}
